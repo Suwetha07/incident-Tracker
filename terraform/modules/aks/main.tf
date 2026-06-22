e99053f5fb8b257b5a213fa74fa5e6e26dba6017
Suwetha <kit.25.21bcs057@gmail.com>
2026-06-22 16:18:48 +0530

    fix: remove role assignment due to lacking owner permissions

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -43,8 +43,8 @@ resource "azurerm_user_assigned_identity" "aks_identity" {
   tags                = var.tags
 }
 
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = var.acr_id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
+# resource "azurerm_role_assignment" "aks_acr_pull" {
+#   scope                = var.acr_id
+#   role_definition_name = "AcrPull"
+#   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+# }
